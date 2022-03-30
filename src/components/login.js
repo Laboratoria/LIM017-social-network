3990a023c1c3f143b8e0755fdba90f1a7c22b947
Nay Trevejo <nay.trevejo@gmail.com>
2022-03-30 17:01:36 -0500

    agregado botones en diferentes vistas del login

--- src/components/login.js
+++ src/components/login.js
@@ -6,12 +6,49 @@ export const login = () => {
   /* bienvenidaDiv.textContent = 'Bienvenida al Login'; */
   const list = `
     <div class="img">
-    <img src="DuckyPets.png" alt="">
+    <img src="imagenes/DuckyPets.png" alt="">
     </div>`;
+
   bienvenidaDiv.insertAdjacentHTML('beforeend', list);
+
+  // Botones de acceso
+  const textoLogin = document.createElement('h2');
+  textoLogin.textContent = 'Iniciar Sesion';
+  bienvenidaDiv.appendChild(textoLogin);
+
+  const buttonGmailLogin = document.createElement('button');
+  buttonGmailLogin.textContent = 'Iniciar Sesion con Gmail';
+  buttonGmailLogin.setAttribute('id', 'btnGmailLogin');
+
+  const buttonFacebookLogin = document.createElement('button');
+  buttonFacebookLogin.textContent = 'Iniciar Sesion con Facebook';
+  buttonFacebookLogin.setAttribute('id', 'btnFacebookLogin');
+
+  const buttonEmailLogin = document.createElement('button');
+  buttonEmailLogin.textContent = 'Iniciar Sesion con Email';
+  buttonEmailLogin.setAttribute('id', 'btnEmailLogin');
+
+  bienvenidaDiv.appendChild(buttonGmailLogin);
+  bienvenidaDiv.appendChild(buttonFacebookLogin);
+  bienvenidaDiv.appendChild(buttonEmailLogin);
+
+  const forgotPassword = document.createElement('p');
+  forgotPassword.textContent = '¿Olvidaste tu contraseña?';
+  bienvenidaDiv.appendChild(forgotPassword);
+
+  // No tienes cuenta
+  const noAccount = document.createElement('p');
+  noAccount.textContent = '¿No tienes cuenta?';
+  bienvenidaDiv.appendChild(noAccount);
+  const noAccountRegister = document.createElement('p');
+  noAccountRegister.textContent = 'Registrate';
+  noAccountRegister.setAttribute('id', 'btnRegistrarse');
+  bienvenidaDiv.appendChild(noAccountRegister);
+
+  // Boton de regresar a la bienvenida
   const buttonBienvenida = document.createElement('button');
 
-  buttonBienvenida.textContent = 'Regresar a la bienvenida';
+  buttonBienvenida.textContent = 'Regresar al inicio';
 
   buttonBienvenida.addEventListener('click', () => onNavigate('/'));
   bienvenidaDiv.appendChild(buttonBienvenida);
--- src/components/register.js
+++ src/components/register.js
@@ -4,9 +4,22 @@ import { onNavigate } from '../main.js';
 export const register = () => {
   const bienvenidaDiv = document.createElement('div');
   bienvenidaDiv.textContent = 'Bienvenida al registro';
+
+  const buttonGmailRegister = document.createElement('button');
+  buttonGmailRegister.setAttribute('id', 'btnGmailLogin');
+  buttonGmailRegister.textContent = 'Registrarse con Gmail';
+
+  const yesAccount = document.createElement('p');
+  yesAccount.textContent = '¿Ya tienes cuenta?';
+  bienvenidaDiv.appendChild(yesAccount);
+  const yesAccountLogin = document.createElement('p');
+  yesAccountLogin.textContent = 'Ingresa';
+  yesAccountLogin.setAttribute('id', 'yesAccount');
+  bienvenidaDiv.appendChild(yesAccountLogin);
+
   const buttonBienvenida = document.createElement('button');
 
-  buttonBienvenida.textContent = 'Regresar a la bienvenida';
+  buttonBienvenida.textContent = 'Regresar al inicio';
 
   buttonBienvenida.addEventListener('click', () => onNavigate('/'));
 
