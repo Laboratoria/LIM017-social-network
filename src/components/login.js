71f858da00059a8f3de38c15aec78483fdf916b3
Emmigumi <20160133@lamolina.edu.pe>
2022-03-30 11:38:41 -0500

    Agregado Template Literal-img, comprobado

--- src/components/login.js
+++ src/components/login.js
@@ -2,7 +2,12 @@ import { onNavigate } from '../main.js';
 
 export const login = () => {
   const bienvenidaDiv = document.createElement('div');
-  bienvenidaDiv.textContent = 'Bienvenida al Login';
+  /* bienvenidaDiv.textContent = 'Bienvenida al Login'; */
+  const list = `
+    <div class="img">
+    <img src="DuckyPets.png" alt="">
+    </div>`;
+  bienvenidaDiv.insertAdjacentHTML('beforeend', list);
   const buttonBienvenida = document.createElement('button');
 
   buttonBienvenida.textContent = 'Regresar a la bienvenida';
