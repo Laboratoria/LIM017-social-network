33f87c116ba1323908f4c79d75c889c9da1380c6
elianamendez1 <elianamendez1809@gmail.com>
2022-04-07 08:56:58 -0500

    Resolve template literals LogIn

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,18 @@
-// eslint-disable-next-line padded-blocks
-// eslint-disable-next-line import/no-cycle
-/* import { onNavigate } from '../main.js'; */
-
 export const Home = () => {
-  const containerLogo = `<figure class="containerLogo">
-                                <img src="Imagenes/Logotipo/Logo-icon.png" alt="Binge Worthy logo" class="topLogo">
-                            </figure>
+  const containerLogo = `<figure class= "containerLogo">
+                          <img src="Imagenes/Logotipo/Logo-icon.png" alt="Binge Worthy logo" class="topLogo">
+                          </figure>
                           <figure class="containerLogoLetters">
-                                <img src="Imagenes/Logotipo/Logo-letters-only.png" alt="Binge Worthy logo" class="appName">
-                                <figcaption>Binge Worthy</figcaption>
-                                <p id="slogan" class="pink">MATCH WITH YOUR STORIES</p>
-                            </figure>`;
-
-  const createAccBtn = '<button id="createAccBtn">Create an account</button>';
-
+                          <img src="Imagenes/Logotipo/Logo-letters-only.png" alt="Binge Worthy logo" class="appName">
+                          <figcaption>Binge Worthy</figcaption>
+                          <p id="slogan" class="pink">MATCH WITH YOUR STORIES</p>
+                          </figure>
+                          <button id="createAccBtn">Create an account</button>`;
   const containerLogin = `<div id="goToLogin">
-                              <p>Already have an account?</p>
-                              <a href="" id="goToLoginBtn" class="pink">Log in</a>
-                              </div>`;
+                          <p>Already have an account?</p>
+                          <a id="goToLoginBtn" class="pink">Log in</a>
+                          </div>`;
 
-  const LogInDivs = containerLogo + createAccBtn + containerLogin;
+  const LogInDivs = containerLogo + containerLogin;
   return LogInDivs;
 };
