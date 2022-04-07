17b94c7963336f0a0479784d90272d285321829e
malev.dg <malev.dg@outlook.com>
2022-04-07 10:56:44 -0500

    modificación primera pantalla

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -2,7 +2,7 @@ import { onNavigate } from '../main.js';
 
 export const Welcome = () => {
 
-  let WelcomeDiv = document.createElement("div");
+  /*let WelcomeDiv = document.createElement("div");
   let buttonRegister = document.createElement("button");
   let buttonLogin = document.createElement("button");
   buttonLogin.className = 'buttonLogin';
@@ -15,6 +15,36 @@ export const Welcome = () => {
 
   WelcomeDiv.appendChild(buttonRegister);
   WelcomeDiv.appendChild(buttonLogin);
+
+  return WelcomeDiv;*/
+
+  let WelcomeDiv = document.createElement("section");
+  WelcomeDiv.innerHTML = `<section id="intro" class="intro">
+  <main class="container">
+  <img class="logotype" src="./img/LogoTellMe.svg">
+  <div>
+    <p id="welcomeApp" class="welcomeApp">¡Bienvenid@!</p>
+    <button id="buttonLogin">Iniciar sesión</button>
+  </div>
+  </main>
+  </section>`
+
+
+  let buttonRegister = document.createElement("button");
+  let buttonLogin = document.createElement("button");
+  buttonRegister.textContent ="Regístrate";
+  buttonRegister.addEventListener("click", () => onNavigate ('/register'));
+
+  /*buttonLogin.setAttribute("id","buttonLogin");*/
+  /*buttonLogin.textContent ="Iniciar sesión";*/
+  buttonLogin.addEventListener('click', () => onNavigate ('/login'));
+
+  buttonRegister.classList.add("buttonRegister");
+
+  WelcomeDiv.appendChild(buttonRegister);
+  /*WelcomeDiv.appendChild(buttonLogin);*/
+  WelcomeDiv.classList.add("WelcomeDiv");
+  WelcomeDiv.querySelector("#buttonLogin").addEventListener('click', () => onNavigate ('/login'));
 
   return WelcomeDiv;
 };
