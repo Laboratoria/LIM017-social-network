37deb6dd1ae3f8095acc1bcf22ba4bb9362a9751
arelia10 <arelia101195@gmail.com>
2022-04-11 17:30:15 -0500

    cambios

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,14 +1,19 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDgirqObjPQ7a3Tn_WUl-227PqIBILPepY",
-  authDomain: "tell-me-9bc01.firebaseapp.com",
-  projectId: "tell-me-9bc01",
-  storageBucket: "tell-me-9bc01.appspot.com",
-  messagingSenderId: "719389346264",
-  appId: "1:719389346264:web:6361bca4a1248fcc7d0aad"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  /*// Your web app's Firebase configuration
+  const firebaseConfig = [
+    apiKey: "AIzaSyDgirqObjPQ7a3Tn_WUl-227PqIBILPepY",
+    authDomain: "tell-me-9bc01.firebaseapp.com",
+    projectId: "tell-me-9bc01",
+    storageBucket: "tell-me-9bc01.appspot.com",
+    messagingSenderId: "719389346264",
+    appId: "1:719389346264:web:6361bca4a1248fcc7d0aad"
+  ];
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
