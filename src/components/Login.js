0d2894fbc9ded4563169756d89c4a15a8ff5df4c
unknown <rousiflores24@gmail.com>
2022-04-12 10:29:24 -0500

    Creación de  vistas de componenetes

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import { onNavigate } from '../main.js';
+export const Login = () => {
+  const HomeDiv = document.createElement("div");
+  HomeDiv.textContent = "Bienvenido al Login";
+  const buttonHome = document.createElement("button");
+  
+
+  buttonHome.textContent = "Regresar al Home";
+  
+   buttonHome.addEventListener('click', () => onNavigate('/'));
+  
+  HomeDiv.appendChild(buttonHome);
+  
+  return HomeDiv;
+};
--- src/main.js
+++ src/main.js
@@ -1,5 +1,51 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
-
+/* import { myFunction } from './lib/index.js';
+ const imprimir = document.getElementById("root");
+ imprimir.innerHTML= `<h1> aca se refleja lo que hagamos</h1>`
 myFunction();
+ */
+
+import { Home } from "./components/Home.js"; 
+import { Register } from "./components/Register.js"; 
+import { Login } from "./components/Login.js"; 
+
+const rootDiv = document.getElementById("root");
+const routes = {
+  "/" : Home,
+  "/register": Register,
+  "/login": Login,
+};
+
+
+/* const component = routes[window.location.pathname];
+rootDiv.appenChild(component()); */
+export const onNavigate = (pathname) => {
+  window.history.pushState(
+    {},
+    pathname,
+    window.location.origin + pathname,
+  );
+   while (rootDiv.firstChild) {
+    rootDiv.removeChild(rootDiv.firstChild);
+  }
+
+  rootDiv.appendChild(routes[pathname]());
+};
+
+const component = routes[window.location.pathname];
+
+window.onpopstate = () => {
+  rootDiv.appendChild(component());
+};
+ /*  while (rootDiv.firstChild) { */
+  /*   rootDiv.removeChild(rootDiv.firstChild);
+  }
+  rootDiv.appendChild(routes[window.location.pathname] *());
+}; */
+
+rootDiv.appendChild(component());
+
+
+
+
