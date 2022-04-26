f4796bdd3f821d2207989db9499d533b171ec069
Emmigumi <20160133@lamolina.edu.pe>
2022-04-26 17:39:32 -0500

    Register.spect.js-test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-cycle */
 import { routes } from './components/Router.js';
 
-const rootDiv = document.getElementById('root');
+/* const rootDiv = document.getElementById('root');
 
 export const onNavigate = (pathname) => {
   window.history.pushState(
@@ -24,4 +24,24 @@ window.onpopstate = () => {
   rootDiv.appendChild(routes[window.location.pathname]());
 };
 
-rootDiv.appendChild(component());
+rootDiv.appendChild(component()); */
+
+export const onNavigate = (pathname) => {
+  const rootDiv = document.getElementById('root');
+  window.history.pushState({}, pathname, window.location.origin + pathname);
+
+  while (rootDiv.firstChild) {
+    rootDiv.removeChild(rootDiv.firstChild);
+  }
+
+  rootDiv.appendChild(routes[pathname]());
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+  const rootDiv = document.getElementById('root');
+  const component = routes[window.location.pathname];
+  window.onpopstate = () => {
+    rootDiv.appendChild(component());
+  };
+  rootDiv.appendChild(component());
+});
