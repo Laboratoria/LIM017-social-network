43cda46aa9c4952e5967a55542c657c4e1894000
ElizabethChillcce <eli.chillcce.08@gmail.com>
2022-05-05 22:38:56 -0500

    importando funciones de autFirebase a init

--- src/lib/application/init.js
+++ src/lib/application/init.js
@@ -1,5 +1,9 @@
 /* eslint-disable import/no-unresolved */
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
 import { firebaseConfig } from './config.js';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification, GoogleAuthProvider, signInWithPopup, signOut, updateProfile } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
+import { app } from './init.js';
+
 
 export const app = initializeApp(firebaseConfig);
+export { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification, GoogleAuthProvider, signInWithPopup, signOut, updateProfile };
