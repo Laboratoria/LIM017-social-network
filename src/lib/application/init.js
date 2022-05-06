646242104c6a2299ccf282ead935cc3d28b4e358
unknown <rousiflores24@gmail.com>
2022-05-05 22:34:34 -0500

    Creación de campo 'post' en vista 'Home.js' , importación de servicio 'collection' en dataFirestore.js

--- src/lib/application/init.js
+++ src/lib/application/init.js
@@ -1,5 +1,30 @@
 /* eslint-disable import/no-unresolved */
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
+import { getFirestore, collection, addDoc } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  sendEmailVerification,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+}
+  from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
+
 import { firebaseConfig } from './config.js';
 
+export {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  sendEmailVerification,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+};
 export const app = initializeApp(firebaseConfig);
+
+export { getFirestore, collection, addDoc };
